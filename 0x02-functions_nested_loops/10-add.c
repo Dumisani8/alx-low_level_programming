c0502466017cb018192b6c87c6f8d80b0a70202b
Dumisani8 <dumisanif@gmail.com>
2023-03-21 13:43:46 -0700

    my commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,15 @@
 #include "main.h"
 /**
 * add - adds two integers and returns the result
-* @a: first integer
-* @b: second integer
 *
-* Return: sum of a and b
+* @a: the first integer to be added
+* @b: the second integer to be added
+*
+* Return: the sum of a and b
 */
 int add(int a, int b)
 {
-return a + b;
+int sum;
+sum = a + b;
+return (sum);
 }
