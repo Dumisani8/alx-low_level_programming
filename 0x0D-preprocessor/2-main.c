cc9d2beb4e4390e1a4d42e20ad42962b55a06911
Dumisani8 <dumisanif@gmail.com>
2023-04-17 12:32:05 -0700

    my commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-int main() {
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
 printf("This program was compiled from the file: %s\n", __FILE__);
-return 0;
+return (0);
 }
